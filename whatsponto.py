db7a8aeb761deb388c9d75eb498eb8deccc4fb8f
Rafael Ramires <rafaelsramires@hotmail.com>
2021-01-11 22:18:12 -0300

    commit

--- whatsponto.py
+++ whatsponto.py
@@ -7,11 +7,20 @@ def gerar_contatos():
     planilha = pd.read_excel('C:\whatsponto\contatos.xlsx')
     contatos = str(planilha)
     contatos = contatos.split('\n')
-    listados = len(contatos)
-    print(contatos)
     del contatos[0]
+    listados = len(contatos)
+    print(listados)
     list_contatos = []
     count_list = 0
-    for word in contatos:
-        list_contatos += [word.split()]
-        return(list_contatos, )
+    lista_erros = ''
+    for word in contatos:     
+        if len(word.split()) == 4:
+            list_contatos += [word.split()]
+        else:
+            lista_erros += 'Erro com ' + word + '\n'
+
+    print(list_contatos) 
+    print(lista_erros)       
+       
+gerar_contatos()
+
